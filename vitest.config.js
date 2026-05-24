f48a4f37080109c02bf51615ca8eda8cf991b98e
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-05-25 00:16:49 +0600

    add react compiler

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './setupTests.js',
-  }
+  },
 });
